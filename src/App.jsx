ef45882a78088c1f6bc66125db09013447239382
Chazz Carrizales <czcarrizales@yahoo.com>
2025-11-12 14:35:06 -0800

    route for PokemonQuiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PokemonDetails from './PokemonDetails'
 import Login from "./Login";
 import Signup from "./Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PokemonQuiz from './PokemonQuiz';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path='/pokedex' element={<PokedexList />} />
           <Route path='/pokemon-details' element={<PokemonDetails />}/>
+          <Route path='/pokemon-quiz' element={<PokemonQuiz />} />
         </Routes>
       </BrowserRouter>
     </div>
